640b4520d95c2a4ebf9af91a6a72cc0c70411589
Solcii <sol081105@gmail.com>
2022-01-21 22:04:48 +0100

    press quit btn

--- js/main.js
+++ js/main.js
@@ -6,9 +6,16 @@ const info_box = document.querySelector('.info-box');
 const quit_btn = info_box.querySelector('.botones .cancel-btn');
 const continue_btn = info_box.querySelector('.botones .start-btn');
 
-/* Al apretar el Start Btn */
+/* Press Start Btn */
 start_btn.onclick = () =>{
     info_box.classList.remove('desactivado');
     principal_screen.classList.add('desactivado');
     body.style.background = '#DCC1B6';
+}
+
+/* Press Quit Btn */
+quit_btn.onclick = () =>{
+    info_box.classList.add('desactivado');
+    principal_screen.classList.remove('desactivado');
+    body.style.background = '#FAFAFA';
 }
